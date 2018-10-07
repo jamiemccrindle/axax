de8027e8a4174f29740b0b121ea058b0eb711875
kedar paritkar <kedar.paritkar@tieto.com>
2018-10-07 20:18:53 +0530

    #27 skipWhile : changes as per our discussion updating the documentation branch too.

--- src/skipwhile.ts
+++ src/skipwhile.ts
@@ -3,10 +3,12 @@
  */
 export function skipWhile<T>(predicate: (t: T) => boolean = () => true) {
   return async function* inner(source: AsyncIterable<T>) {
+    let skipping = false;
     for await (const item of source) {
-      if (!predicate(item)) {
-        yield item;
-      }
+        if (skipping || !predicate(item)) {
+          skipping = true;
+          yield item;
+        }
     }
   };
 }
