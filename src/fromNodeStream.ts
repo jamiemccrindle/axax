278510aea0d5a9a70201c539c780a8a183c9e59a
Jamie McCrindle <jamiemccrindle@gmail.com>
2019-09-29 13:12:37 +0100

    generic

--- src/fromNodeStream.ts
+++ src/fromNodeStream.ts
@@ -1,10 +1,10 @@
 import fs from "fs";
 import { Subject } from "./subject";
 
-export function fromNodeStream(stream: fs.ReadStream) {
-  const subject = new Subject<Buffer | string>();
+export function fromNodeStream<T = Buffer | string>(stream: fs.ReadStream) {
+  const subject = new Subject<T>();
 
-  stream.on("data", (chunk: Buffer | string) => subject.onNext(chunk));
+  stream.on("data", (chunk: T) => subject.onNext(chunk));
 
   stream.on("end", () => subject.onCompleted());
   stream.on("error", e => subject.onError(e));
